98e5ded036148c9a136d1afdf0e4908aca8e7820
noman-projects <163643709+noman-projects@users.noreply.github.com>
2024-04-06 21:25:53 +0500

    index

---
+++ app.js
@@ -0,0 +1,29 @@
+document.getElementsByClassName("fa-brands fa-youtube")[0].addEventListener("click",function(){
+    window.open("https://www.youtube.com/")
+})
+
+document.getElementsByClassName("fa-brands fa-youtube")[1].addEventListener("click",function(){
+    window.open("https://web.whatsapp.com/")
+})
+
+document.getElementsByClassName("fa-brands fa-youtube")[2].addEventListener("click",function(){
+    window.open("https://www.youtube.com/")
+})
+
+
+document.getElementsByClassName("fa-brands fa-youtube")[3].addEventListener("click",function(){
+    window.open("https://www.linkedin.com/in/noman-afridi-798689245/")
+})
+
+document.getElementsByClassName("fa-brands fa-youtube")[4].addEventListener("click",function(){
+    window.open("https://www.youtube.com/")
+})
+
+document.getElementsByClassName("fa-brands fa-youtube")[5].addEventListener("click",function(){
+    window.open("https://www.youtube.com/")
+})
+
+document.getElementsByClassName("fa-brands fa-youtube")[6].addEventListener("click",function(){
+    window.open("https://www.youtube.com/")
+})
+
